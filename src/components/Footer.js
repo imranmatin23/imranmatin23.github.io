7e16cc30dff8091bdf8d85368ad3e0490eca0021
Imran Matin <imranmatin23@gmail.com>
2023-05-03 17:09:17 -0700

    Create simple Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,14 @@ import "../styles/Footer.css";
 
 function Footer() {
   return (
-    <div >
+    <div className="footer">
+      <div className="socialMedia">
+        {/* TODO: Use Icons */}
+        <p>Email</p>
+        <p>LinkedIn</p>
+        <p>Github</p>
+        <p>Twitter</p>
+      </div>
       <p> &copy; 2023 Imran Matin</p>
     </div>
   );
